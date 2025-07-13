d7cc72a9998cdda616c68251030c846ba3bda6a5
AgusLatre <aguslatre855@gmail.com>
2025-07-13 15:48:55 -0300

    Update api.php
    Route to see al patients

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PatientController;
+use App\Models\Patient;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/patients/register', [PatientController::class, 'store']);
+
+Route::get('/patients', function () {
+    return response()->json(Patient::all());
+});
